9b0d049961ec965dddbda9d2578a0d4257750994
hangtt-29 <hangtt29@onemount.com>
2021-08-31 09:54:53 +0700

    add bai 1

---
+++ src/main/java/eneties/Main.java
@@ -0,0 +1,12 @@
+package eneties;
+
+import java.util.Collection;
+
+public class Main {
+    public static void main(String[] args) {
+        CountString c= new CountString();
+        c.countWords();
+        c.stringNormalization();
+
+    }
+}
